ba316f0f216c56eeaec6871e00fba74ca9b259a3
Dmitry-fq <frizhikini.d@gmail.com>
2022-10-02 15:42:00 +0500

    Добавлен пробный вход панель администрирования

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstTest {
+    private WebDriver driver;
+
+    @Test
+    public void myFirstTest() {
+        driver = new ChromeDriver();
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        driver.quit();
+    }
+}
