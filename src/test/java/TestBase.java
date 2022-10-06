d8822a23b5c38b9beabdf5b3dcd45b01444774be
Dmitry-fq <frizhikini.d@gmail.com>
2022-10-06 20:43:21 +0500

    Добавил в TestBase неявные ожидания

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -5,14 +5,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
+import java.time.Duration;
+
 public class TestBase {
 
     public WebDriver driver;
 
     @Before
     public void start() {
-            driver = new ChromeDriver();
-        }
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
 
     @After
     public void stop() {
