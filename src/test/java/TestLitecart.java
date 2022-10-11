955eb54adb7a2854511dd8760a337631747e2eee
Dmitry-fq <frizhikini.d@gmail.com>
2022-10-12 00:16:43 +0500

    Изменил локатор для товаров в testStickersDisplayed

--- src/test/java/TestLitecart.java
+++ src/test/java/TestLitecart.java
@@ -35,27 +35,20 @@ public class TestLitecart extends TestBase {
 
         List<WebElement> menu = driver.findElements(By.id("app-"));
         for (int i = 0; i < menu.size(); i++) {
-            System.out.println("Цикл:" + i);
-            System.out.println("Menu:" + menu.get(i).getText());
-
             menu.get(i).click();
             System.out.println(h1Present());
             Assertions.assertTrue(h1Present());
             //TODO убрать дублирование кода в menu и submenu
             menu = driver.findElements(By.id("app-"));
             List<WebElement> submenu = menu.get(i).findElements(By.tagName("li"));
-            System.out.println("Размер submenu:" + submenu.size());
             for (int j = 0; j < submenu.size(); j++) {
                 submenu.get(j).click();
                 menu = driver.findElements(By.id("app-"));
                 submenu = menu.get(i).findElements(By.tagName("li"));
                 System.out.println(h1Present());
                 Assertions.assertTrue(h1Present());
-                System.out.println("submenu " + (j + 1));
             }
-            driver.navigate().back();
             menu = driver.findElements(By.id("app-"));
-            System.out.println("_______________________");
         }
     }
 
@@ -64,10 +57,11 @@ public class TestLitecart extends TestBase {
     public void testStickersDisplayed() {
         this.goToMain();
 
-        List<WebElement> products = driver.findElements(By.className("image-wrapper"));
+        List<WebElement> products = driver.findElements(By.className("product"));
         for (int i = 0; i < products.size(); i++) {
             List<WebElement> stickers = products.get(i).findElements(By.className("sticker"));
             Assertions.assertEquals(stickers.size(), 1);
+            System.out.println(stickers.size());
         }
     }
 }
